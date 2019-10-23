467fb001485510ab8c8dab5991ce305f2f5bc7cd
rtanubra <rdtanubrata@gmail.com>
2019-10-23 07:30:46 -0400

    finished setup for database. Now migration can be done through deploy or migrate:production

--- postgrator-config.js
+++ postgrator-config.js
@@ -4,4 +4,5 @@ module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
   "connectionString": (process.env.NODE_ENV === 'test')? process.eventNames.env.TEST_DATABSE_URL : process.env.DATABASE_URL,
+  "ssl": !!process.env.SSL,
 }
